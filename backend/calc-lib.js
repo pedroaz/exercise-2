48a9c05183ac2f654df7c9db44ef0e058f92a027
pedroaz <pedro.azvm@gmail.com>
2022-09-21 19:56:06 -0300

    Initial Commit

---
+++ backend/calc-lib.js
@@ -0,0 +1,21 @@
+
+module.exports = class Calculator{
+
+    soma(x, y){
+        
+    }
+
+    subtracao(x, y){
+
+    }
+
+    divisao(x, y) {
+  
+    
+    }
+
+    multiplicacao(x, y){
+
+    }
+    
+}
