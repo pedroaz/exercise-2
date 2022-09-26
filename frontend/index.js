cbd11b6aac3dc08eb416fe3147afebce03bafffe
LukasMansoreitch <lukasmansoreitch@hotmail.com>
2022-09-26 18:58:41 -0300

    aasasas

--- frontend/index.js
+++ frontend/index.js
@@ -6,10 +6,23 @@
     let result = document.getElementById('result');
     let url = 'http://localhost:3000'
 
+
     if (op == "soma"){
         url = url + `/soma?x=${n1}&y=${n2}`
         const res = await axios.get(url)  
-        result.innerHTML = `resultado: ${res.data.result}`
+        result.innerHTML = `resultado: ${res.data.resultado}`
+    } else if (op == "sub"){
+        url = url + `/sub?x=${n1}&y=${n2}`
+        const res = await axios.get(url)  
+        result.innerHTML = `resultado: ${res.data.resultado}`
+    } else if (op == "div"){
+        url = url + `/div?x=${n1}&y=${n2}`
+        const res = await axios.get(url)  
+        result.innerHTML = `resultado: ${res.data.resultado}`
+    } else {
+        url = url + `/multi?x=${n1}&y=${n2}`
+        const res = await axios.get(url)  
+        result.innerHTML = `resultado: ${res.data.resultado}`
     }
 } 
 
